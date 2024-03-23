72cc288b5babaaa9a25cc43b997ba64f71b77e5c
Teshaa <tk.wanja@gmail.com>
2024-03-23 15:52:53 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}></View>
+      <View style={{ flexDirection: "row", padding: 20, width: 200 }}></View>
+
+      <View
+        style={{
+          backgroundColor: "white",
+          alignContent: "flex-end",
+        }}
+      >
+        <View>
+          <Text>My Diary</Text>
+        </View>
+        <Text> today </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "flex-end",
   },
 });
